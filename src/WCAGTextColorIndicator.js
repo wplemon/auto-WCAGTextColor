83017e0fb6af7d2a6cb2010e1c9900cd25a8d8aa
Ari Stathopoulos <aristath@gmail.com>
2019-12-21 11:36:55 +0200

    bugfix

--- src/WCAGTextColorIndicator.js
+++ src/WCAGTextColorIndicator.js
@@ -40,48 +40,48 @@ const WCAGTextColorIndicator = ( props ) => {
 	const styles = reactCSS( {
 		default: {
 			selectedColorWrapper: {
-				'padding-bottom': '12px',
+				paddingBottom: '12px',
 				display: 'grid',
-				'grid-template-columns': 'max-content 1fr',
-				'grid-gap': '12px'
+				gridTemplateColumns: 'max-content 1fr',
+				gridGap: '12px'
 			},
 
 			selectedColorIndicator: {
 				width: '30px',
 				height: '30px',
-				'border-radius': '50%',
+				borderRadius: '50%',
 				display: 'block',
-				'background-color': props.value,
-				'box-shadow': '#000 2px 2px 5px -3px inset'
+				backgroundColor: props.value,
+				boxShadow: '#000 2px 2px 5px -3px inset'
 			},
 
 			selectedColorIndicatorWrapper: {
 				display: 'flex',
-				'align-items': 'center',
-				'justify-content': 'center',
-				'flex-direction': 'column'
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexDirection: 'column'
 			},
 
 			selectedColorIndicatorText: {
-				'font-size': '10px',
-				'font-weight': '700',
-				'font-family': 'Menlo, Consolas, monaco, monospace',
+				fontSize: '10px',
+				fontWeight: '700',
+				fontFamily: 'Menlo, Consolas, monaco, monospace',
 				width: 'max-content'
 			},
 
 			ratingIndicator: {
-				'border-radius': '3px',
+				borderRadius: '3px',
 				padding: '3px 10px',
-				'background-color': getRatingBackgroundColor(),
+				backgroundColor: getRatingBackgroundColor(),
 				color: '#fff',
-				'font-weight': '700',
-				'font-size': '10px'
+				fontWeight: '700',
+				fontSize: '10px'
 			},
 
 			table: {
-				'font-size': '10px',
+				fontSize: '10px',
 				width: '100%',
-				'line-height': '1'
+				lineHeight: '1'
 			},
 
 			td: {
@@ -97,14 +97,16 @@ const WCAGTextColorIndicator = ( props ) => {
 				<p style={ styles.selectedColorIndicatorText }>{ props.value }</p>
 			</div>
 			<table style={ styles.table }>
-				<tr>
-					<td style={ styles.td }>{ props.i18n.a11yRating }</td>
-					<td style={ styles.td }><span style={ styles.ratingIndicator }>{ getRating() }</span></td>
-				</tr>
-				<tr>
-					<td style={ styles.td }>{ props.i18n.contrastBg }</td>
-					<td style={ styles.td }>{ getContrastBackground() }</td>
-				</tr>
+				<tbody>
+					<tr>
+						<td style={ styles.td }>{ props.i18n.a11yRating }</td>
+						<td style={ styles.td }><span style={ styles.ratingIndicator }>{ getRating() }</span></td>
+					</tr>
+					<tr>
+						<td style={ styles.td }>{ props.i18n.contrastBg }</td>
+						<td style={ styles.td }>{ getContrastBackground() }</td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
 	);
